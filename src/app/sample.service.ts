00c192baae1b53f5ea9407139bc1d5535033c934
deyan <deyan.koba@zupit.it>
2024-08-05 17:52:20 +0200

    update sample service

--- src/app/sample.service.ts
+++ src/app/sample.service.ts
@@ -1,13 +1,18 @@
 import {Injectable} from '@angular/core';
 import {delay, Observable, of} from "rxjs";
+import {map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SampleService {
 
-  getData(): Observable<string[]> {
+  getData(): Observable<{ id: number, name: string }[]> {
     const data = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-    return of(data).pipe(delay(1000));
+    return of(data)
+      .pipe(
+        map((items) => items.map((item, idx) => ({id: idx, name: item}))),
+        delay(500)
+      );
   }
 }
